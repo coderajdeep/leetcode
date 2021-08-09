5edd60c2a8494055ab5752f7f9ae7a8c1ade3633
Rajdeep Mallick <87999365+coderajdeep@users.noreply.github.com>
2021-08-09 09:10:30 +0530

    283. Move Zeroes
    https://leetcode.com/problems/move-zeroes/

---
+++ 283.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int index = 0;
+        for(int i=0; i<n; ++i) {
+            if(nums[i] != 0)
+                swap(nums[i], nums[index++]);
+        }
+    }
+};
